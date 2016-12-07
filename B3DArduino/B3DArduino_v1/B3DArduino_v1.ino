e498e08aa6204849f87b946b6cf2fef61efb961f
Tavius Koktavy <tavius.koktavy@gmail.com>
2016-12-07 07:33:17 -0500

    Add files via upload

---
+++ B3DArduino/B3DArduino_v1/B3DArduino_v1.ino
@@ -0,0 +1,139 @@
+/*
+ * Break Out 3D - CC Final Project Draft
+ * Tavius Koktavy
+ */
+
+/*
+ * DRAFTING BOARD - What do I need from the Arduino side?
+ *  - (CHECK) Accelerometer input
+ *  - (CHECK) Gyroscope input
+ *  - Transmission to server
+ *  - Listening for response data
+ *    - Real world response (LED)
+ * 
+ */
+
+
+
+#include "CurieIMU.h"
+
+void setup() {
+  Serial.begin(9600); // initialize Serial communication
+  //while (!Serial);    // wait for the serial port to open
+  while (Serial.available() <= 0) {
+    Serial.println("hello"); // send a starting message
+    delay(300);              // wait 1/3 second
+  }
+  // initialize device
+  Serial.println("Initializing IMU device...");
+  CurieIMU.begin();
+  CurieIMU.setAccelerometerRate(25);
+
+
+  // Set the accelerometer range to 16G
+  CurieIMU.setAccelerometerRange(2);
+}
+
+    //float ax, ay, az;   //scaled accelerometer values
+    int ax, ay, az;
+
+void loop() {
+  
+  // Scaled or nah?
+  CurieIMU.readAccelerometer(ax, ay, az);
+  
+  //CurieIMU.readAccelerometerScaled(ax, ay, az);
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+    Serial.print(ax + .1);
+    Serial.print(",");
+    Serial.println(ay - .01);
+//   Serial.print(",");
+//    Serial.println(az - .1);
+//    Serial.write(inByte);
+//    analogWrite(9, inByte);
+//    Serial.print("hello");
+//    Serial.print("\r");
+//    Serial.print("\n");
+
+  }
+
+  // read accelerometer measurements from device, scaled to the configured range
+
+  // display tab-separated accelerometer x/y/z values
+//  Serial.print(ax + .1);
+//  Serial.print(",");
+//  Serial.print(ay - .01);
+//  Serial.print(",");
+//  Serial.print(az - .1);
+//  Serial.println();
+//  delay(50);
+}
+
+
+
+//
+//
+//
+//// Necessary for board function
+//#include "CurieIMU.h"
+//
+//
+//
+//void setup() {
+//  Serial.begin(9600); // initialize Serial communication
+//  while (!Serial);    // wait for the serial port to open
+//
+//  // initialize device
+//  Serial.println("Initializing IMU device...");
+//  CurieIMU.begin();
+//
+//  // Set the accelerometer range to 2G
+//  CurieIMU.setAccelerometerRange(2);
+//  // Set the gyroscope range to 250 degrees/second
+//  CurieIMU.setGyroRange(250); // Scale hand speed to this
+//
+//  // See RawImuDataSerial for calibration on setup?
+//}
+//
+//// Accel sample loop() code, baseline test
+//void loop() {
+//  float ax, ay, az;   //scaled Accel values
+//  float gx, gy, gz; //scaled Gyro values
+//
+//  // Accellerometer data for orientation, scaled to the configured range
+//  CurieIMU.readAccelerometerScaled(ax, ay, az);
+//
+//  // display tab-separated accelerometer x/y/z values
+//  Serial.print("a:\t");
+//  Serial.print(ax);
+//  Serial.print("\t");
+//  Serial.print(ay);
+//  Serial.print("\t");
+//  Serial.print(az);
+//  Serial.println();
+//
+//  // Gyroscope data for movement, scaled to the configured range
+//  CurieIMU.readGyroScaled(gx, gy, gz);
+//
+//  // display tab-separated gyro x/y/z values
+//  Serial.print("g:\t");
+//  Serial.print(gx);
+//  Serial.print("\t");
+//  Serial.print(gy);
+//  Serial.print("\t");
+//  Serial.print(gz);
+//  Serial.println();
+//}
+//}
+
+
+
+//void loop() {
+//
+//  // read accelerometer:
+//  int x = CurieIMU.readAccelerometer(X_AXIS);
+//  int y = CurieIMU.readAccelerometer(Y_AXIS);
+//  int z = CurieIMU.readAccelerometer(Z_AXIS);
+//
+//}
---
+++ B3DArduino/B3DArduino_v2/B3DArduino_v2.ino
@@ -0,0 +1,85 @@
+/*
+ * Break Out 3D - CC Final Project Draft
+ * Tavius Koktavy
+ */
+
+/*
+ * DRAFTING BOARD - What do I need from the Arduino side?
+ *  - (CHECK) Accelerometer input
+ *  - (CHECK) Gyroscope input
+ *  - Transmission to server
+ *  - Listening for response data
+ *    - Real world response (LED)
+ * 
+ */
+
+
+
+#include "CurieIMU.h"
+#include <SoftwareSerial.h>
+
+//SoftwareSerial mySerial(2, 3); // RX, TX
+
+void setup() {
+  Serial.begin(115200); // initialize Serial communication
+  
+  while (Serial.available() <= 0) {
+    Serial.println("Hello, world? Iterating until response."); // send a starting message
+    delay(300);              // wait 1/3 second
+  }
+  
+  // initialize device
+  Serial.println("Initializing IMU device...");
+  CurieIMU.begin();
+  CurieIMU.setAccelerometerRate(1600); // Does this aid in smoothing or nah?
+
+  // Set the accelerometer range in G's
+  CurieIMU.setAccelerometerRange(4);
+}
+
+//float ax, ay, az;   //scaled accelerometer values
+int ax, ay, az;
+
+//void(* resetFunc) (void) = 0;  // declare reset fuction at address 0
+
+void loop() {
+  
+  // Scaled or nah?
+  //CurieIMU.readAccelerometerScaled(ax, ay, az);
+  CurieIMU.readAccelerometer(ax, ay, az);
+  
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+    // 'c' resets the Arduino if the game is won
+    if (inByte == 'c') {
+      while (Serial.available() <= 0) {
+        Serial.println("Hello, world? Iterating until response."); // send a starting message
+        delay(300);              // wait 1/3 second
+  }
+    }
+    else {
+      Serial.print(ax);
+      Serial.print(",");
+      Serial.println(ay);
+    }
+
+//   Serial.print(",");
+//    Serial.println(az);
+//    Serial.write(inByte);
+//  analogWrite(9, inByte); // Should light up the LED?
+//    Serial.print("hello");
+//    Serial.print("\r");
+//    Serial.print("\n");
+  }
+
+  // read accelerometer measurements from device, scaled to the configured range
+
+  // display tab-separated accelerometer x/y/z values
+//  Serial.print(ax + .1);
+//  Serial.print(",");
+//  Serial.print(ay - .01);
+//  Serial.print(",");
+//  Serial.print(az - .1);
+//  Serial.println();
+//  delay(50);
+}
